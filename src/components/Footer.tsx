f8ddde28eb1a32cdd52edaf48031d5cee58e6610
figoathallah <figoirawan02@gmail.com>
2025-08-19 14:16:36 +0700

    small footer style change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <footer className="relative bg-sky-200 text-white p-4">
       <div className="text-center pb-4">
-        <p className="text-gray-900">Contact: belajarlinkedin@gmail.com</p>
+        <p className="text-gray-900 italic">
+          Contact: belajarlinkedin@gmail.com
+        </p>
       </div>
       <div className="social-links text-center space-x-5">
         <SocialIcon url="https://www.instagram.com/belajarlinkedin/" />
