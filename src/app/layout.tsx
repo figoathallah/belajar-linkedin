85e6cb78dcbf71609ad0618e9744103212541d7e
figoathallah <figoirawan02@gmail.com>
2025-08-19 23:10:32 +0700

    add features section structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import { ReactNode } from "react";
 import "./globals.css";
+import { Barlow } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
+const barlow = Barlow({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-barlow",
+});
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,7 +16,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className={`flex flex-col min-h-screen ${barlow.variable}`}>
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <html>
       <body>
-        <main className="pt-20 flex-1 relative flex flex-col items-center justify-center text-center px-4">
+        <main className="pt-20 flex-1 relative flex flex-col items-center justify-center text-center px-4 pb-6">
           <img
             src="office-work-placeholder.jpg"
             alt="office illustration"
@@ -42,6 +42,40 @@ export default function Home() {
             </a>
           </div>
         </main>
+        <div className="text-center pb-6 py-4">
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-balance text-white">
+            Key Benefits
+          </h1>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 px-4 py-5 pb-10 text-center">
+            <div className="bg-blue-200 p-4 rounded-md text-gray-700">
+              <h2 className="font-barlow font-semibold text-lg sm:text-xl lg:text-2xl pb-3">
+                Optimized LinkedIn Profile
+              </h2>
+              <p>
+                Ketahui beberapa cara praktis untuk membuat profil LinkedIn kamu
+                lebih menarik di mata HRD!
+              </p>
+            </div>
+            <div className="bg-indigo-200 p-4 rounded-md text-gray-700">
+              <h2 className="font-barlow font-semibold text-lg sm:text-xl lg:text-2xl pb-3">
+                Job Hunting Strategies
+              </h2>
+              <p>
+                Pahami berbagai macam strategi yang paling ampuh dalam mencari
+                pekerjaan!
+              </p>
+            </div>
+            <div className="bg-sky-200 p-4 rounded-md text-gray-700">
+              <h2 className="font-barlow font-semibold text-lg sm:text-xl lg:text-2xl pb-3">
+                Networking Tips
+              </h2>
+              <p>
+                Dapatkan wawasan terkait cara membangun relasi yang cepat dan
+                bermutu!
+              </p>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
